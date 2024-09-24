925d13f3b5fb8b1e63622f1dea0dda0e263559ed
has673 <hassansajjas>
2024-09-24 17:45:56 +0500

    f

--- src/Components/Row.tsx
+++ src/Components/Row.tsx
@@ -5,7 +5,7 @@ const Row: React.FC<{ image: string; text: string }> = ({ image, text }) => {
   return (
     <div className="flex justify-center  gap-2 p-2 my-3">
       <Image className="ml-10" src={image} alt="e" width={30} height={10} />
-      <h5 className="mt-3">{text}</h5>
+      <h5 className="mt-3 px-2">{text}</h5>
     </div>
   );
 };
---
+++ src/app/data/array3.ts
@@ -0,0 +1,13 @@
+const array3 = [
+  {
+    icon: "/8.png",
+    heading: "For Collage Students",
+    text: "Who want to rocket-start their career and get placed in thier dream companies",
+  },
+  {
+    icon: "/9.png",
+    heading: "For Working Professionals",
+    text: "Who wish to shift to top product based companies and ace thier tech careers",
+  },
+];
+export default array3;
